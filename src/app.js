cb09582980db11df9a5839be440fd8ba50bd2e8e
Davi Garms <davigarms@gmail.com>
2020-02-04 20:21:20 +0000

    Add key prop

--- src/app.js
+++ src/app.js
@@ -2,12 +2,17 @@
 
 import React, { Component } from 'react'
 import Title from './title'
+import Square from './square'
 
 class App extends Component {
   render () {
     return (
       <div className='container'>
         <Title name='Davi' idade={39} />
+
+        {['blue', 'red', 'green'].map((color) => (
+          <Square key={color} color={color} />
+        ))}
       </div>
     )
   }
