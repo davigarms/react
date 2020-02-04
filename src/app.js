1b2e656c7c07703451683fd6448c0b71b83d01e8
Davi Garms <davigarms@gmail.com>
2020-02-04 13:54:42 +0000

    Add attributes in render
    It introduces the use of HTML attributes

--- src/app.js
+++ src/app.js
@@ -6,8 +6,10 @@ import Title from './title'
 const App = React.createClass({
   render: function () {
     return (
-      <div>
+      <div className='container'>
         <Title name='Davi' />
+        <label htmlFor='input' data-label='label'>Input</label>
+        <input type='text' id='input' aria-hidden={true} />
       </div>
     )
   }
