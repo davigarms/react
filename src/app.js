59740666a47960299a1cdcc6e58defff48543e03
Davi Garms <davigarms@gmail.com>
2020-02-09 18:09:00 +0000

    Introduce States

--- src/app.js
+++ src/app.js
@@ -1,15 +1,23 @@
 'use strict'
 
 import React, { Component } from 'react'
-import LikeButton from './likebutton'
-import SearchButton from './searchbutton'
 
 class App extends Component {
+  constructor () {
+    super()
+    this.state = {
+      text: 'Davi'
+    }
+  }
+
   render () {
     return (
-      <div className='container'>
-        <LikeButton />
-        <SearchButton />
+      <div
+        className='container' onClick={() => this.setState({
+          text: 'Outro texto'
+        })}
+      >
+        {this.state.text}
       </div>
     )
   }
